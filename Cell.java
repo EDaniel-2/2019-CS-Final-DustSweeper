013a2fc7f41bee5dfc65923a984eb892ca99caf5
EDaniel-2 <46804432+EDaniel-2@users.noreply.github.com>
2019-06-19 16:41:16 -0700

    ayy b this boi work more

--- Cell.java
+++ Cell.java
@@ -11,76 +11,58 @@ import javafx.scene.image.ImageView;
 import java.util.*;
 import java.io.*;
 import java.lang.*;
-public class Cell extends PlayGame{
-   Button tile;
-   int x;
-   int y;
-   int rows;
-   int cols; 
-   //boolean isMine;
+import javafx.scene.layout.StackPane;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+
+public class Cell extends StackPane{
+   Button tile = new Button();
+   int row;
+   int col;
+   boolean isMine;
+   int minesAround;
       
-   public Cell(Button tile, int x, int y){
-      this.tile = tile;
-      this.x = x;
-      this.y = y;
-      this.rows = rows;
-      this.cols = cols;
-      //this.isMine = isMine;
-      tile.setOnMouseClicked(mouseHandler);
+   public Cell(int row, int col, boolean isMine, int minesAround, int size){
+      this.row = row;
+      this.col = col;
+      this.isMine = isMine;
+      this.minesAround = minesAround;
+      tile.setOnMouseClicked(e -> {onClick(e);});
+      tile.setMinHeight(size);
+      tile.setMinWidth(size);
+      getChildren().addAll(tile);
+      setTranslateX(row * size);
+		setTranslateY(col * size);
+      revealMinesAround(); // change this later
+   }
+   
+   private void onClick(MouseEvent e) {
+      System.out.println("In onClick");
+      if (e.getButton() == MouseButton.PRIMARY) {
+         //is left click
+         System.out.println("Primary (" + row + ", " + col + ")");
       }
-   EventHandler<javafx.scene.input.MouseEvent> mouseHandler = new EventHandler<javafx.scene.input.MouseEvent>() {
- 
-        @Override
-        public void handle(javafx.scene.input.MouseEvent mouseEvent) {
-            onClick(mouseEvent);
-            System.out.println("hi");
-         }
-      };
-
-
-   public void onClick(javafx.scene.input.MouseEvent e) {
-   System.out.println("0");
-      if (e.getButton() == javafx.scene.input.MouseButton.PRIMARY) {//is left click
-      System.out.println("1");
-      //System.out.println(row);
-         /*for(int placeX = 0; placeX < rows; placeX++){
-         System.out.println("2");
-            for(int placeY = 0; placeY < cols; placeY++){
-            System.out.println("3");*/
-               if(firstClick == true){//This checks if its the first click which is protected
-                  if(cells[x][y]){//So, if the persons first click is on mine, it places a new mine
-                     setSingleMine();
-                     updateAround(x,y,-1);
-                     cells[changedMine[0]][changedMine[1]] = true;
-                     updateAround(changedMine[0],changedMine[1], 1);
-                     cells[x][y] = false;
-                     System.out.println("6");
-                     //can use minesAround array 
-                     //set mines around for cells around to minesAround-1
-                     //set number of mines for cells around array
-                     //can use setText to check if this works
-                  }
-                  firstClick = false;   
-               }
-            }
-            
-      if(cells[x][y]) {
-         System.out.println("u done");
-         endgame();   
-      }
-      // Right Click
       else {
+         // Right Click
+         System.out.println("Right");
          
          //afterButtonClick();
       }
    }
-   public void revealMineNumber(int placeX, int placeY){
-      if(minesAround[placeX][placeY] == 0){
-         //Dont Show anything 
-      }
-      else{
-         //shownumber and 
-      
+
+   public void revealMinesAround()
+   {
+      if(!isMine){
+         tile.setText(""+minesAround);
       }
    }
+//    public void revealMineNumber(int placeX, int placeY){
+//       if(minesAround[placeX][placeY] == 0){
+//          //Dont Show anything 
+//       }
+//       else{
+         //shownumber and 
+      
+//       }
+//    }
 }
--- SetupBoard.java
+++ SetupBoard.java
@@ -10,42 +10,58 @@ import javafx.scene.image.ImageView;
 import java.util.*;
 import java.io.*;
 import java.lang.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
+
 public class SetupBoard extends Application{
-   int mines;
-   int row;
-   int col;
-   boolean[][] cells;
-   boolean firstClick = true;
+   private static int mines;
+   private static int size;
+   private static boolean[][] isMine;
+   private static boolean firstClick = true;
    //Image dust = new Image(getClass().getResourceAsStream("dust-cloud30.jpg"));
-   Button[][] board;
-   int[][] minesAround;
+   //Button[][] board;
+   private static int[][] minesAround;
    Scene scene;
    GridPane panel = new GridPane();
-   int[] changedMine = new int[2];
+   //int[] changedMine = new int[2];
+   private static Stage main;
+   private static VBox vbox = new VBox();
+   private static Cell[][] cells;
+   public static int btnSize = 50;
 
+   @Override
    public void start(Stage primaryStage){
       Scanner console = new Scanner(System.in);
+      main = primaryStage;
       System.out.println("This is an adaption of minesweeper called dustsweeper.");
-      System.out.print("How many rows would you like? (recommended 18) ");
-      this.row = console.nextInt();
-      System.out.print("How many columns would you like? (recommended 18) ");
-      this.col = console.nextInt();
-      System.out.print("How many mines would you like? (recommended 22) ");
-      this.mines = console.nextInt();
-      this.board = new Button[row][col];
-      this.cells = new boolean[row][col];
-      this.minesAround = new int[row][col];
+      System.out.print("How many rows / columns would you like? (recommended 9) ");
+      size = console.nextInt();
+      System.out.print("How many mines would you like? (recommended 8) ");
+      mines = console.nextInt();
+      cells = new Cell[size][size];
+      //this.board = new Button[rows][cols];
+      isMine = new boolean[size][size];
+      minesAround = new int[size][size];
       setMines(mines);
-      for(int x = 0; x < row; x++){
-         for(int y = 0; y < col; y++){
-            createCell(x,y);    
+      //System.out.println(Arrays.deepToString(isMine).replace("], ", "]\n"));
+      for(int row = 0; row < size; row++){
+         for(int col = 0; col < size; col++){
+            // System.out.println("checkAround("+row+","+col+")");
+            int mines = checkAround(row, col);
+            // System.out.println("found "+mines+" mines");
+            minesAround[row][col]=mines;
+            // System.out.println("minesaround saved");
          }
       }
-      //scene.setOnMouseClicked(mouseHandler);   
-      this.scene = new Scene(panel, col*50, row*50);    
-      primaryStage.setTitle("DustSweeper");
-      primaryStage.setScene(scene);
-      primaryStage.show();
+      
+      vbox.getChildren().addAll(setupCells());
+      //this.scene = new Scene(panel, cols*btnSize, rows*btnSize);    
+      scene = new Scene(vbox);
+      main.setTitle("DustSweeper");
+      main.setScene(scene);
+      main.setResizable(false);
+      main.sizeToScene();
+      main.show();
    }
       
    public static void main(String[] args){
@@ -57,47 +73,58 @@ public class SetupBoard extends Application{
          setSingleMine();
       }
    }
-   
-   public void createCell(int x, int y){
-      board[x][y] = new Button();
-      minesAround[x][y] =  checkAround(x,y);
-      board[x][y].setText(""+minesAround[x][y]);
-      //board[x][y].setGraphic(new ImageView(dust));
-      board[x][y].setPrefSize(50, 50);
-      this.panel.add(board[x][y],x,y);
-      Cell cell = new Cell(board[x][y], x, y); 
-   }
-   
-   public boolean accessCell(int x, int y){
-      return cells[x][y];
-   }
-   
+
    public void setSingleMine(){
-      int placeX = (int) (Math.random()*row);
-      int placeY = (int) (Math.random()*col);
-      if(this.cells[placeX][placeY] == true){setSingleMine();}
+      int placeRow = (int) (Math.random()*size);
+      int placeCol = (int) (Math.random()*size);
+      if(isMine[placeRow][placeCol] == true){setSingleMine();}
       else{
-         this.cells[placeX][placeY] = true;
-         changedMine[0] = placeX;
-         changedMine[1] = placeY;
+         isMine[placeRow][placeCol] = true;
+         // changedMine[0] = placeX;
+         // changedMine[1] = placeY;
       }
    }
+
+ //   public void createCell(int x, int y){
+//       board[x][y] = new Button();
+//       minesAround[x][y] =  checkAround(x,y);
+//       board[x][y].setText(""+minesAround[x][y]);
+//       //board[x][y].setGraphic(new ImageView(dust));
+//       board[x][y].setPrefSize(50, 50);
+//       this.panel.add(board[x][y],x,y);
+//       Cell cell = new Cell(board[x][y], x, y); 
+//    }
    
-   public void updateAround(int x, int y, int update){
-      for(int rowX = x-1; rowX < x+1; rowX++){//Goes through a 3 by three block and updates
-         for(int colY = y-1; colY < y+1; colY++){
-            minesAround[rowX][colY] = minesAround[rowX][colY]+update;
-         }
-      }
-   }
+   // public boolean accessCell(int x, int y){
+   //    return isMine[x][y];
+   // }
    
-   public int checkAround(int x, int y){
+   
+   // public void updateAround(int x, int y, int update){
+   //    for(int rowX = x-1; rowX < x+1; rowX++){//Goes through a 3 by three block and updates
+   //       for(int colY = y-1; colY < y+1; colY++){
+   //          minesAround[rowX][colY] = minesAround[rowX][colY]+update;
+   //       }
+   //    }
+   // }
+   
+   public int checkAround(int row, int col){
+      // System.out.println("checkAround("+row+","+col+")  rows="+rows+"  cols="+cols);
       int minesAround = 0;
-      for(int startX = x-1; startX <= x+1; startX++){
-         for(int startY = y-1; startY <= y+1; startY++){
-            if(startX < 0 || startX >= row || startY < 0 || startY >= col){}
-            else{
-               if(cells[startX][startY] == true){
+      boolean rowValid;
+      boolean colValid;
+      boolean notSameValid;
+      for(int checkRow = row-1; checkRow <= row+1; checkRow++){
+         for(int checkCol = col-1; checkCol <= col+1; checkCol++){
+            // System.out.println("check ("+checkRow+", "+checkCol+ ")");
+            rowValid = (0 <= checkRow && checkRow < size);
+            colValid = (0 <= checkCol && checkCol < size);
+            notSameValid = (checkRow!=row || checkCol!=col);
+            // System.out.println("rowValid:"+rowValid+", colValid:"+colValid+", notSameValid:"+notSameValid);
+            if(rowValid && colValid  && notSameValid){
+               // System.out.println("checkAround("+row+","+col+") isMine["+checkRow+"]["+checkCol+"]");
+               // System.out.println("isMine["+checkRow+"]["+checkCol+"]="+isMine[checkRow][checkCol]);
+               if(isMine[checkRow][checkCol] == true){
                   minesAround++;
                }
             }   
@@ -106,6 +133,18 @@ public class SetupBoard extends Application{
       return minesAround;
    }
 
+   private static Parent setupCells(){ //
+      Pane root = new Pane();
+      root.setPrefSize(size*btnSize, size*btnSize);
+      for(int row = 0; row < size; row++){
+         for(int col = 0; col < size; col++){
+            Cell cell = new Cell(row,col,isMine[row][col],minesAround[row][col],btnSize);
+            cells[row][col] = cell;
+            root.getChildren().add(cell);  
+         }
+      }
+      return root;
+   }
    public void showEndgame(){
    
    }   
